9227df850fb7ad1d7f8d8089fe37e0f768b8c798
Martynas Mickevičius <self@2m.lt>
2025-11-04 20:08:07 +0200

    secrets and calibre web

--- justfile
+++ justfile
@@ -1,5 +1,16 @@
 fetch:
-    rsync -chavzP --stats root@46.226.107.71:/etc/nixos .
+    rsync -chavzP --stats root@ssh.lab.2m.lt:/etc/nixos .
 
 push:
-    rsync -chavzP --stats ./nixos/ root@46.226.107.71:/etc/nixos/
+    rsync -chavzP --stats ./nixos/ root@ssh.lab.2m.lt:/etc/nixos/
+
+build:
+    ssh root@ssh.lab.2m.lt -t 'nixos-rebuild switch'
+
+dev-prep:
+    nix profile add github:nixos/nixpkgs#nixd
+    nix profile add github:nixos/nixpkgs#nixfmt
+    nix profile add github:ryantm/agenix
+
+encrypt secret:
+    cd nixos; env EDITOR=hx RULES=./secrets/secrets.nix agenix -e {{secret}}.age
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,7 +1,24 @@
-{ pkgs, lib, config, ... }: {
-  imports = [ ./gandicloud.nix ];
+{
+  pkgs,
+  config,
+  ...
+}:
+{
+  imports = [
+    ./gandicloud.nix
+    "${builtins.fetchTarball "https://github.com/ryantm/agenix/archive/main.tar.gz"}/modules/age.nix"
+  ];
 
-  environment.systemPackages = with pkgs; [ neovim htop tailscale ];
+  environment.systemPackages = with pkgs; [
+    bat
+    neovim
+    btop
+    tailscale
+    dua
+  ];
+
+  programs.fish.enable = true;
+  users.defaultUserShell = pkgs.fish;
 
   networking.firewall = {
     enable = true;
@@ -18,13 +35,55 @@
 
   services.caddy = {
     enable = true;
+    virtualHosts."http://lab.2m.lt".extraConfig = ''
+      header Content-Type text/html
+      respond "
+        <h3>Welcome to the 2m lab</h3>
+        <dl>
+          <dt><a href='http://irc.2m.lt'>http://irc.2m.lt</a></dt>
+          <dd>The Lounge IRC Web client</dd>
+          <dt><a href='http://books.2m.lt'>http://books.2m.lt</a></dt>
+          <dd>Calibre Web book library</dd>
+        </dl>
+      "
+    '';
     virtualHosts."http://irc.2m.lt".extraConfig = ''
       reverse_proxy http://localhost:${toString config.services.thelounge.port}
+    '';
+    virtualHosts."http://books.2m.lt".extraConfig = ''
+      reverse_proxy http://localhost:${toString config.services.calibre-web.listen.port}
     '';
   };
 
   services = {
     tailscale.enable = true;
     thelounge.enable = true;
+    calibre-web = {
+      enable = true;
+      options = {
+        enableBookUploading = true;
+        calibreLibrary = "/var/lib/calibre-library";
+      };
+    };
+  };
+
+  age.secrets.restic_password.file = ./secrets/restic_password.age;
+  age.secrets.restic_repository.file = ./secrets/restic_repository.age;
+
+  services.restic.backups = {
+    borgbase = {
+      initialize = true;
+      paths = [
+        "/var/lib"
+      ];
+      passwordFile = config.age.secrets.restic_password.path; # encryption
+      repositoryFile = config.age.secrets.restic_repository.path;
+
+      timerConfig = {
+        # when to backup
+        OnCalendar = "00:05";
+        RandomizedDelaySec = "5h";
+      };
+    };
   };
 }
