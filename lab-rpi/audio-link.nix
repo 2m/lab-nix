c553a67d07d0076fba8e9389861121dabcd95195
Martynas Mickevičius <self@2m.lt>
2026-03-17 11:07:13 +0200

    always restart audio link

--- lab-rpi/audio-link.nix
+++ lab-rpi/audio-link.nix
@@ -14,6 +14,12 @@ in
     serviceConfig = {
       ExecStart = "${pkgs.alsa-utils}/bin/alsaloop -C ${soundIn} -P ${soundOut} -r 48000 -c 2 -f S16_LE -l 4800";
       Restart = "on-failure";
+      RestartSec = "5s";
+    };
+
+    unitConfig = {
+      # never give up restarting, sometimes the usb device might not be there
+      StartLimitIntervalSec = 0;
     };
   };
 
